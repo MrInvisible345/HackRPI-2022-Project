d712f09e9f85e7101865dbf58798c652ed4f858a
MrInvisible345 <97348344+MrInvisible345@users.noreply.github.com>
2022-11-05 11:59:21 -0400

    Start

---
+++ HackRPI 2022 Space Project .py
@@ -0,0 +1,5 @@
+"""
+    Made by Chris Scutero, Viane Matsibekker, Joseph Park, and Rohan Nethala
+    This program will predict space stocks based on past results using machine learning algorithms
+"""
+
