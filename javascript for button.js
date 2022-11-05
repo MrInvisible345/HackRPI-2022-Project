7851f240f721811a3d71a1d952abcb4703abea74
viane-matsibekker <117529189+viane-matsibekker@users.noreply.github.com>
2022-11-05 13:24:24 -0700

    Update javascript for button.js

--- javascript for button.js
+++ javascript for button.js
@@ -1,6 +1,8 @@
 function clickButton(){
   var user_input = parseInt(document.getElmementbyId("user_form").value);
   console.log(user_input)
+
   // upload the results onto the html file
+  return user_input;
   
 }
